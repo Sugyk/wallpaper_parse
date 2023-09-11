61c56ad2eca491cb7bf0684defd00cdcc1978c58
Oleg Zaharov <active.sugyk@gmail.com>
2023-09-11 20:52:46 +0300

    feature: several pages downloading

--- parse.py
+++ parse.py
@@ -68,16 +68,20 @@ class Parse:
             last_page = self.explore_last_page()
             if last_page == -1:
                 print('Try other prompts(example: pixel art): ', end='')
-        print(f'Choose the page (1-{last_page}): ', end='')
+        print(f'Choose the page 1-{last_page} (example: 3-15): ', end='')
         while True:
-            page_str = input()
-            if page_str == 'exit':
-                return -1
-            elif page_str.isdigit() and 1 <= int(page_str) <= last_page:
-                break
-            else:
-                print(f'Input must be integer positive number >=1 and <={last_page}["exit" to abort]: ')
-        self.page = int(page_str)
+            page_str = input().split('-')
+            if len(page_str) == 2:
+                for i in range(2):
+                    if page_str[i].isdigit() and 1 <= int(page_str[i]) <= last_page:
+                        page_str[i] = int(page_str[i])
+                    else:
+                        break
+                else:
+                    if page_str[0] <= page_str[1]:
+                        break
+            print(f'Incorrect input (example: 3-15): ')
+        self.start, self.end = page_str
         while True:
             path = self.get_upload_path()
             if path == -1:
@@ -90,7 +94,6 @@ class Parse:
         return 0
 
     def tread_get_urls(self, start, end):
-        print(self, start, end)
         for tag in range(start, end):
             soup = bs(self.session.get(self.tags[tag]['href'] + '/download').text, 'html.parser')
             self.urls[tag] = soup.find('img', itemprop='contentUrl')['src']
@@ -112,23 +115,16 @@ class Parse:
         for i in range(threads_number):
             threads[i].join()
 
-
-
     def uploading(self, start, end):
-        print(self, start, end)
         filename = '-'.join(self.prompt.split())
         for url_pos in range(start, end):
             image = self.session.get(self.urls[url_pos]).content
             with open(f'{self.upload_path}/{filename}_p{self.page}_{url_pos}.jpg', 'wb') as pict:
                 pict.write(image)
-            print(f'Image {self.download}/{len(self.urls)} downloaded')
+            print(f'Page {self.page}: Image {self.download}/{len(self.urls)} downloaded')
             self.download += 1
 
-    def upload_images(self):
-        if self.configure() == -1:
-            print('Aborting...')
-            return -1
-        print(f'Downloading into: {self.upload_path}')
+    def upload_page(self):
         print('Getting urls...')
         self.get_pages_urls()
         page_length = len(self.urls)
@@ -136,10 +132,24 @@ class Parse:
         ranges = [[i * page_length // threads_number, (i + 1) * page_length // threads_number] for i in range(threads_number)]
         ranges[-1][-1] += page_length % threads_number
         self.download = 1
+        threads = []
         for i in range(threads_number):
-            threading.Thread(group=None, target=self.uploading, args=(ranges[i])).start()
-        threading.Thread(group=None, target=lambda: input, args=(), daemon=True).start()
+            threads.append(threading.Thread(target=self.uploading, args=(ranges[i])))
+        for i in range(threads_number):
+            print(f'Thread {i} is running!')
+            threads[i].start()
+        for i in range(threads_number):
+            threads[i].join()
+    
+    def upload(self):
+        if self.configure() == -1:
+            print('Aborting...')
+            return -1
+        print(f'Downloading into: {self.upload_path}')
+        for i in range(self.start, self.end + 1):
+            self.page = i
+            self.upload_page()
         
 if __name__ == '__main__':
     parser = Parse()
-    parser.upload_images()
+    parser.upload()
